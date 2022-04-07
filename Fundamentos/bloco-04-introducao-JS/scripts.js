57c83b9e4f0aa6e9e50b366e51ec92bf38547b27
Davi Albino <negroalbino_@hotmail.com>
2022-04-07 11:33:35 -0300

    Todas as operações matemáticas

--- Fundamentos/bloco-04-introducao-JS/scripts.js
+++ Fundamentos/bloco-04-introducao-JS/scripts.js
@@ -2,5 +2,13 @@ const a = 36;
 const b = 97;
 
 let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
 
-console.log(adicao)
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
